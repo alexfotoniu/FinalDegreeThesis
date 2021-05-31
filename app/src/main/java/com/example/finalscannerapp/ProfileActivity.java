cd1d62edc59e694be694d450ea8100be470e2bd5
Alex Fotoniu <alexfotoniu@gmail.com>
2021-05-31 15:23:25 +0300

    logout functionality implemented

--- app/src/main/java/com/example/finalscannerapp/ProfileActivity.java
+++ app/src/main/java/com/example/finalscannerapp/ProfileActivity.java
@@ -1,15 +1,32 @@
 package com.example.finalscannerapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class ProfileActivity extends AppCompatActivity {
+
+    private Button logoutButton;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setTheme(R.style.Theme_FinalScannerApp);
         setContentView(R.layout.activity_profile);
+
+        logoutButton = (Button)findViewById(R.id.logout_button);
+        logoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(ProfileActivity.this, LoginActivity.class));
+            }
+        });
     }
 }
