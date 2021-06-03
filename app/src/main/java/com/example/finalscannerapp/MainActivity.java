e9587d144d125eaf8f1403ad46ac38a3892f160c
Alex Fotoniu <alexfotoniu@gmail.com>
2021-06-03 20:18:59 +0300

    toolbar added, main activity menu modified

--- app/src/main/java/com/example/finalscannerapp/MainActivity.java
+++ app/src/main/java/com/example/finalscannerapp/MainActivity.java
@@ -1,14 +1,23 @@
 package com.example.finalscannerapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.zip.Inflater;
 
 public class MainActivity extends AppCompatActivity {
 
-    private BottomNavigationView bottomNavigationView;
+    private FloatingActionButton floatingActionButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,9 +25,27 @@ public class MainActivity extends AppCompatActivity {
         setTheme(R.style.Theme_FinalScannerApp);
         setContentView(R.layout.activity_main);
 
-        bottomNavigationView = (BottomNavigationView)findViewById(R.id.bottom_navigation_view);
-        bottomNavigationView.setBackground(null);
-        bottomNavigationView.getMenu().getItem(1).setEnabled(false);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
+        floatingActionButton = (FloatingActionButton) findViewById(R.id.fab);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.miProfile:
+                startActivity(new Intent(MainActivity.this, ProfileActivity.class));
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
