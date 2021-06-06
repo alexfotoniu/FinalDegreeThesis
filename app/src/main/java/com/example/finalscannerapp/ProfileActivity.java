2e2e09ab3804c8cb2c4ddbe6cadb0ea1263ea15d
Alex Fotoniu <alexfotoniu@gmail.com>
2021-06-06 22:39:29 +0300

    showing profile datas implemented

--- app/src/main/java/com/example/finalscannerapp/ProfileActivity.java
+++ app/src/main/java/com/example/finalscannerapp/ProfileActivity.java
@@ -4,15 +4,37 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import org.jetbrains.annotations.NotNull;
+import org.w3c.dom.Text;
 
 public class ProfileActivity extends AppCompatActivity {
 
     private Button logoutButton;
+    private TextView textViewGreeting;
+    private TextView textViewEmailAdress;
+    private TextView textViewFullName;
+    private TextView textViewAge;
+
+    private FirebaseUser user;
+    private DatabaseReference reference;
+
+    //logged in user ID
+    private String userID;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -20,7 +42,39 @@ public class ProfileActivity extends AppCompatActivity {
         setTheme(R.style.Theme_FinalScannerApp);
         setContentView(R.layout.activity_profile);
 
-        logoutButton = (Button)findViewById(R.id.logout_button);
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        reference = FirebaseDatabase.getInstance().getReference("Users");
+        userID = user.getUid();
+
+        final TextView textViewGreeting = (TextView) findViewById(R.id.greeting);
+        final TextView textViewEmailAdress = (TextView) findViewById(R.id.email_adress);
+        final TextView textViewFullName = (TextView) findViewById(R.id.full_name);
+        final TextView textViewAge = (TextView) findViewById(R.id.age);
+
+        reference.child(userID).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull @NotNull DataSnapshot snapshot) {
+                User userProfile = snapshot.getValue(User.class);
+
+                if (userProfile != null) {
+                    String fullName = userProfile.fullName;
+                    String email = userProfile.email;
+                    String age = userProfile.age;
+
+                    textViewGreeting.setText("Welcome, " + fullName.toUpperCase() + " !");
+                    textViewFullName.setText(fullName.toUpperCase());
+                    textViewEmailAdress.setText(email);
+                    textViewAge.setText(age);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull @NotNull DatabaseError error) {
+                Toast.makeText(ProfileActivity.this, "Something wrong happend", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        logoutButton = (Button) findViewById(R.id.logout_button);
         logoutButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
