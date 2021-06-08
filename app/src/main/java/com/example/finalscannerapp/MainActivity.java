e42393b182bdd483d860ff3dfb4e12a6f8d3a9b9
Alex Fotoniu <alexfotoniu@gmail.com>
2021-06-08 20:32:39 +0300

    back button added to profile activity

--- app/src/main/java/com/example/finalscannerapp/MainActivity.java
+++ app/src/main/java/com/example/finalscannerapp/MainActivity.java
@@ -4,18 +4,23 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.zip.Inflater;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private FloatingActionButton floatingActionButton;
 
@@ -29,6 +34,7 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         floatingActionButton = (FloatingActionButton) findViewById(R.id.fab);
+        floatingActionButton.setOnClickListener(this);
 
     }
 
@@ -48,4 +54,26 @@ public class MainActivity extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.fab:
+//                try {
+//                    checkPermission();
+//                    startActivity(new Intent(MainActivity.this, Class.forName(MediaStore.ACTION_IMAGE_CAPTURE)));
+//                } catch (ClassNotFoundException e) {
+//                    e.printStackTrace();
+//                }
+                Toast.makeText(MainActivity.this, "Click!", Toast.LENGTH_LONG).show();
+                break;
+        }
+    }
+
+
+//    public void checkPermission() {
+//        if (checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+//            showCameraPreview();
+//        }
+//    }
 }
--- app/src/main/java/com/example/finalscannerapp/ProfileActivity.java
+++ app/src/main/java/com/example/finalscannerapp/ProfileActivity.java
@@ -46,6 +46,7 @@ public class ProfileActivity extends AppCompatActivity {
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
 
         user = FirebaseAuth.getInstance().getCurrentUser();
